4366e8f7b368ea737c8b2dec6ecbc0a4b96ceda1
GillyWallace <gillianwallace6@gmail.com>
2020-11-06 20:33:38 -0500

    update

--- db/conn.php
+++ db/conn.php
@@ -9,10 +9,14 @@
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
     try{
-        $pdo = new PDO($dsn, $user, $pass);
-        $pdo->setAttribute(PDO::ATTR_ERR,ODE, PDO::ERRMODE_EXCEPTION);
-        
+        $pdo = new PDO($dsn, $user, $psss);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
     }catch(PDOException $e){
         throw new PDOException($e->getMessage());
     }
+
+    require_once 'crud.php';
+    $crud = new crud($pdo);
+
 ?>
