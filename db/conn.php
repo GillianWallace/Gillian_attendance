2fbd209419bc6efb1d50e93bb3370b26ef4d2039
GillyWallace <gillianwallace6@gmail.com>
2020-11-06 19:03:04 -0500

    update

--- db/conn.php
+++ db/conn.php
@@ -9,8 +9,9 @@
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
     try{
-        $pdo = new PDO($dsn, $user, $psss);
-        echo 'Hello DataBase';
+        $pdo = new PDO($dsn, $user, $pass);
+        $pdo->setAttribute(PDO::ATTR_ERR,ODE, PDO::ERRMODE_EXCEPTION);
+        
     }catch(PDOException $e){
         throw new PDOException($e->getMessage());
     }
--- db/crud.php
+++ db/crud.php
@@ -2,19 +2,56 @@
     class crud{
         //private database
         private $db;
+
         //constructor to initialize private to the database connection
         function __construct($conn){
             $this->db = $conn;
         }
+
         //function to insert a new record into the attendee database
         public function insert($fname, $lname, $dob, $email, $contact, $specialty){
-        try {
+            try {
+                //define sql statement to be executed
                 $sql = "INSERT INTO attendee VALUES (:fname,:lname,:dob,:email,:contact,:specialty)";
+                //prepare the sql statement for execution
+                $stmt = $this->db->prepare($sql);
+                //bind all placeholders to the actual values
+                $stmt->bindparam(':fname',$fname);
+                $stmt->bindparam(':lname',$lname);
+                $stmt->bindparam(':dob',$dob);
+                $stmt->bindparam(':email',$email);
+                $stmt->bindparam(':contact',$contact);
+                $stmt->bindparam(':specialty',$specialty);
+                //execute statement
+                $stmt->is_executable();
+                return true;
+            } catch(PDOException $e) {
+                echo $e->getMessage();
+                return false;
             }
-
         }
     }
-
-
-
+        
 ?>
+
+
+
+
+
+           
+        
+
+        
+            
+                
+     
+        
+
+        
+
+        
+           
+                
+
+            
+                
